81af0dfbfd1462f7c76f091aaffed9b633269086
AlexCsorba <124453983+AlexCsorba@users.noreply.github.com>
2023-04-04 15:48:41 -0400

    finished player

--- src/war/Player.java
+++ src/war/Player.java
@@ -1,4 +1,22 @@
 package war;
 
+import java.util.ArrayList;
 public class Player {
+    private ArrayList<Card> hand;
+
+    public Player() {
+        hand = new ArrayList<Card>();
+    }
+
+    public void takeCard(Card aCard) {
+        hand.add(aCard);
+    }
+
+    public Card playCard() {
+        return hand.remove(0);
+    }
+
+    public int cardsInHand() {
+        return hand.size();
+    }
 }
