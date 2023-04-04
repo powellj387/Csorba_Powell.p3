2052a8c2b4238a81f4938fbdd52ca356b2047adc
powellj387 <powellj387@lynchburg.edu>
2023-04-04 15:52:05 -0400

    initial

--- src/war/Deck.java
+++ src/war/Deck.java
@@ -3,13 +3,17 @@ package war;
 public class Deck {
     private int size;
     private ULStack<Card> cards;
-    private Card.Suit[] orderedSuits = {};
-    private Card.Rank[] orderedRanks = {};
+private Card.Suit[] orderedSuits = {Card.Suit.HEARTS, Card.Suit.CLUBS, Card.Suit.DIAMONDS, Card.Suit.SPADES};
+    private Card.Rank[] orderedRanks = {Card.Rank.KING, Card.Rank.QUEEN, Card.Rank.JACK, Card.Rank.TEN,
+            Card.Rank.NINE, Card.Rank.EIGHT, Card.Rank.SEVEN, Card.Rank.SIX, Card.Rank.FIVE, Card.Rank.FOUR,
+            Card.Rank.THREE, Card.Rank.TWO, Card.Rank.ACE};
     public Deck(){
-        cards = new ULStack<>(52);
-        for(Card.Suit suit: Card.Suit.values()){
-            for(Card.Rank rank: Card.Rank.values()){
-                cards.add()
+
+        cards = new ULStack<Card>();
+        for(Card.Suit suit: orderedSuits){
+            for(Card.Rank rank: orderedRanks){
+                Card newCard = new Card(rank, suit);
+                cards.push(newCard);
             }
         }
     }
@@ -19,11 +23,11 @@ public class Deck {
     }
 
     public Card dealCard(){
-
+        return cards.pop();
     }
 
     public int size(){
-        return size;
+        return cards.size();
     }
 
     public boolean empty(){
--- src/war/MS1Main.java
+++ src/war/MS1Main.java
@@ -1,28 +1,28 @@
-/*
+
 package war;
 
 import static java.lang.System.out;
 
 import java.util.Random;
 
-*/
+
 /**
  * This class is used to test your project for Milestone 1.
  * @author Joe Meehean
  *
- *//*
+ */
 
 public class MS1Main {
 	
 	public static void main(String[] args) {
-		//testUnshuffled();
-		testShuffled(2);
+		testUnshuffled();
+		/*testShuffled(2);*/
 	}
 	
-	*/
+
 /**
 	 * Prints out the contents of an unshuffled deck
-	 *//*
+	 */
 
 	public static void testUnshuffled() {
 		Deck deck = new Deck();
@@ -31,18 +31,17 @@ public class MS1Main {
 		}
 	}
 
-	*/
 /**
 	 * Prints out the contents of a deck shuffled with the given seed.
 	 * @param seed
-	 *//*
+	 */
 
-	public static void testShuffled(int seed) {
+/*	public static void testShuffled(int seed) {
 		Deck deck = new Deck();
 		deck.shuffle( new Random(seed) );
 		while( deck.size() != 0 ) {
 			out.println(deck.dealCard());
 		}
-	}
+	}*/
 }
-*/
+
