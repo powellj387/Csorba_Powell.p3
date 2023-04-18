fcf3017cc329230230fed9fc8d9f0524e8c34987
powellj387 <powellj387@lynchburg.edu>
2023-04-17 22:26:19 -0400

    consoleInt

--- src/war/ConsoleInterface.java
+++ src/war/ConsoleInterface.java
@@ -9,7 +9,7 @@ public class ConsoleInterface {
     }
 
     public void newGame(){
-
+        output.println("A new game has started");
     }
     public void displayPostBattleScores(int player1Score, int player2Score){
         output.println("Scores => P1: "+player1Score+"\tP2: "+player2Score);
@@ -20,7 +20,10 @@ public class ConsoleInterface {
     }
 
     public void displayWar(int p1CardsPlayed, Card p1FaceUp, int p2CardsPlayed, Card p2FaceUp){
-
+        for(int i=0; i< p1CardsPlayed;++i) {
+            output.println("X\tX");
+        }
+        output.println(p1FaceUp+"\t"+p2FaceUp);
     }
 
     public void displayGameOver(int player1Score, int player2Score){
